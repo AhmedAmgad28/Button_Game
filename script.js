cd40fffb9bcb82cb992c7f4d4eb2c3c7a2d4bafd
AhmedAmgad28 <ahmed.amgad282001@yahoo.com>
2022-12-22 19:04:50 +0200

    Commit 2

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ var disp1=document.getElementById("display1");
 var disp2=document.getElementById("display2");
 let P1Score = 0;
 let P2Score = 0;
-let ptime = 5;
+// let P1time = 5;
+// let P2time = 5;
+// var timer1=document.getElementById("timer1");
+// var timer2=document.getElementById("timer2");
 let logbtn1 = document.getElementById("Log1");
 let logbtn2 = document.getElementById("Log2");
-//let P1time = document.getElementById("displayT1");
-//let P2time = document.getElementById("displayT2");
+
 function P1logout(){
     if (logbtn1.innerHTML == 'Log Out'){
         logbtn1.innerHTML = 'Log in';
@@ -34,22 +36,6 @@ function P2logout(){
         p2.disabled=false;
     }
 };
-function logout(){
-
-}
-// //Get Score for each player
-// var count1=0;
-// var count2=0;
-// var disp1=document.getElementById("display1");
-// var disp2=document.getElementById("display2");
-// function button1(){
-//     count1++;
-//     disp1.innerHTML=count1;
-// };
-// function button2(){
-//     count2++;
-//     disp2.innerHTML=count2;
-// };
 
 
 function Game(){
@@ -57,21 +43,59 @@ function Game(){
     confirm("Note that your turn is 5 seconds only , if you did not click the button you will not get a point");
     alert("Game started");
 
-    function countdown(){
+    function Swap(p){
+        if (p == p1){
+            p1.disabled=true;
+            p2.disabled=false;
+        }
+        else if (p == p2){
+            p2.disabled=true;
+            p1.disabled=false;
+        }
+    }
 
-        p1.addEventListener("click",function(){
-            P1Score +=1;
-            disp1.innerHTML=P1Score;
-        });
-        p2.addEventListener("click",function(){
-            P2Score +=1;
-            disp2.innerHTML=P2Score;
-        });
+    p1.addEventListener("click",function(){
+        P1Score = P1Score + 1 ;
+        disp1.innerHTML=P1Score;
+        Swap(p1);
+    });
+    p2.addEventListener("click",function(){
+        P2Score  = P2Score + 1 ;
+        disp2.innerHTML=P2Score;
+        Swap(p2);
+    });
+
+    // function P1CountDown(){
+    //     P1time -=1;
+    //     timer1.innerHTML=P1time;
+    //     if(P1time == -1){
+    //         timer1.innerHTML=0;
+    //         clearInterval(P1counter);
+    //         P1Score -=1;
+    //     }
+    // }
+    // let P1counter = setInterval(P1CountDown,1000);
+
+    // function P2CountDown(){
+    //     P2time -=1;
+    //     timer2.innerHTML=P1time;
+    //     if(P2time == -1){
+    //         timer2.innerHTML=0;
+    //         clearInterval(P2counter);
+    //         P2Score -=1;
+    //     }
+    // }
+    // let P2counter = setInterval(P2CountDown,1000);
+
+
+    function countdown(){
         gametime -=1;
         maintimer.innerHTML=gametime;
         if(gametime == 0){
             maintimer.innerHTML=gametime;
             clearInterval(counter);
+            p1.disabled=true;
+            p2.disabled=true;
             if(P1Score > P2Score){
                 alert("Player 1 WON !!!");
                 P1Title.innerHTML="WINNER !!!";
@@ -88,30 +112,4 @@ function Game(){
         }
     }
     let counter = setInterval(countdown,1000);
-
-    // function Playertime(p){ 
-    //     ptime -=1;
-    //     turn(p);
-    //     console.log(`Player timer ${ptime}`)
-    //     if(ptime == 0){
-    //         clearInterval(PlayerCounter);
-    //     }
-    // }
-    // let PlayerCounter = setInterval(Playertime(1),1000);
-    
-    // // var player = 1;
-    // function turn(player){
-    //     if( player == 1){
-    //         p1.addEventListener("click",function(){
-    //             P1Score += 1 ;
-    //             clearInterval(PlayerCounter);
-    //         });
-    //         player=2;
-    //     }
-    //     else{
-    //         p2.addEventListener("click",p2turn);
-    //         player=1
-    //     }
-    // }
-
 }
